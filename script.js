6da94af104830f224bd80adfc413c2626df9e350
Surya Vinay Kumar <111567764+vinay348@users.noreply.github.com>
2025-11-10 21:15:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const byteSize = (str) => {
   // write your code here
-	  return new Blob([str]).size;
-
+  return new Blob([str]).size;
 };
 
 // Do not change the code below
 const str = prompt("Enter some string.");
 
-alert(byteSize(str.length));
+alert(byteSize(str));   // ✅ FIXED: pass string, not str.length
